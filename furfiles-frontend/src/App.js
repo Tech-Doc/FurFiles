15ab6e41a1c95473a59770ba29afb765cdfd5743
tech-doc <dr.stanleyperpetual@gmail.com>
2024-05-29 16:26:17 +0100

    App.js

--- furfiles-frontend/src/App.js
+++ furfiles-frontend/src/App.js
@@ -24,26 +24,3 @@ function App() {
 
 export default App;
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import DataPage from './components/DataPage';
-import AboutUs from './components/AboutUs';
-import OurServices from './components/OurServices';
-
-function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Switch>
-                    <Route path="/login" component={LoginPage} />
-                    <Route path="/data" component={DataPage} />
-                    <Route path="/about" component={AboutUs} />
-                    <Route path="/services" component={OurServices} />
-                </Switch>
-            </div>
-        </Router>
-    );
-}
-
-export default App;
