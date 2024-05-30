7ca0b5ade77d512ad4cbd1ba5c41f3ed5f627201
tech-doc <dr.stanleyperpetual@gmail.com>
2024-05-30 12:19:29 +0100

    App.js

--- furfiles-frontend/src/App.js
+++ furfiles-frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import DataPage from './components/DataPage';
+import AboutUs from './components/AboutUs';
+import OurServices from './components/OurServices';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/login" component={LoginPage} />
+                    <Route path="/data" component={DataPage} />
+                    <Route path="/about" component={AboutUs} />
+                    <Route path="/services" component={OurServices} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
