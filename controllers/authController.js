9b31e36ae9e423b7c2c669600a3f518dcb953a31
Tech-Doc <dr.stanleyperpetual@gmail.com>
2024-06-04 16:32:13 +0100

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+const AuthService = require('../services/authService');
+
+exports.register = async (req, res) => {
+    try {
+        const user = await AuthService.register(req.body);
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const token = await AuthService.login(req.body);
+        res.json({ token });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
