778da15865bc84f75217e5c2c8bdbf9066f8e6ad
Tech-Doc <dr.stanleyperpetual@gmail.com>
2024-06-04 16:19:59 +0100

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,19 @@
-const Redis = require('ioredis');
-const redis = new Redis();
+const redis = require('redis');
+const dotenv = require('dotenv');
 
-module.exports = redis;
+dotenv.config();
+
+const client = redis.createClient({
+    port: process.env.REDIS_PORT,
+});
+
+client.on('connect', () => {
+    console.log('Redis client connected');
+});
+
+client.on('error', (err) => {
+    console.error('Redis error: ', err);
+});
+
+module.exports = client;
 
